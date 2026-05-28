6bd4df8ebf9de6a22f3786cc5e8d881d4a1aa2c3
intotherain0102 <intotherain0102@gmail.com>
2026-05-28 13:39:43 +0900

    웹크롤링 연습2

--- 오늘의유머.py
+++ 오늘의유머.py
@@ -9,12 +9,12 @@ import re
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 # 파일에 저장(write text)
-f = open("clien.txt", "wt", encoding="utf-8")
+f = open("todayHumor.txt", "wt", encoding="utf-8")
 
 #url = "https://www.clien.net/service/board/sold"
 
-# 페이지 번호를 0부터 9까지 증가시키면서 반복
-for i in range(0, 11):
+# 페이지 번호를 1부터 10까지 증가시키면서 반복
+for i in range(1, 11):
     url = "https://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=" + str(i)
 
     print(url)
@@ -25,20 +25,18 @@ for i in range(0, 11):
     soup = BeautifulSoup(data, "html.parser")
 
     # filtering
-    list = soup.find_all("span", {"data-role":"list-title-text"})
+    list = soup.find_all("td", attrs={"class":"subject"})
 
     for tag in list:
-        title = tag.text.strip()
-        # 문자열 가공
-        title = title.replace("\n", "")
+        title = tag.find("a").text.strip()
+
         # 정규표현식으로 문자열 가공
-        if re.search("아이폰", title):
+        if re.search("일본", title):
             print(title)
             f.write(title + "\n")
 
 f.close()
 
 # 선택한 block 주석 : cntl + /
-# <span class="subject_fixed" data-role="list-title-text" title="미개봉) 아이폰 Xs 데빌케이스 Type X2">
-# 							미개봉) 아이폰 Xs 데빌케이스 Type X2
-# 						</span>
+# <td class="subject">
+# <a href="/board/view.php?table=bestofbest&amp;no=482962&amp;s_no=482962&amp;page=1" target="_top">"요즘 시대에서 한국인을 나누는 기준"</a>
